19d60417d41b955c78c1c449ac13f6ca4c940806
Hyunuk Kang <whg1011w@gmail.com>
2026-07-26 18:01:16 +0900

    "feat: 프로젝트 초기 설정 및 jsoup 의존성 추가"

---
+++ src/main/java/com/livedata/hub/LiveDataHubApplication.java
@@ -0,0 +1,13 @@
+package com.livedata.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDataHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveDataHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livedata/hub/LiveDataHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livedata.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDataHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
